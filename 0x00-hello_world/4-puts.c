31034dfb90684e9bd1d22d6614d018ba81f1bf93
Abigail-cloud <toluabby12@gmail.com>
2022-12-08 17:06:59 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: 0number
+ */
+int main(void)
+		{
+		char Mystr[] = "Programming is like building a multilingual puzzle\n";
+		puts(Mystr);
+		return (0);
+		}
