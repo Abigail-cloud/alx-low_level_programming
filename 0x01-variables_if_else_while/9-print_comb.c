44b151dbf3a1ba6b7d9fb4ac6961bf218017c2da
Abigail-cloud <toluabby12@gmail.com>
2022-12-09 18:37:04 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,8 @@ int main(void)
 		{
 			putchar(x);
 			if (x != '9')
-			{
-																	putchar(',');
-				putchar(' ');											}			
+			{														putchar(',');
+				putchar(' ');											}
 			x++;
 		}
 
