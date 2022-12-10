a31c8f208263b2185f877ccff5249b5d6c0ea8b6
Abigail-cloud <toluabby12@gmail.com>
2022-12-09 18:31:53 -0800

    9-print_comb.c"

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: combination og sigle digits
+ */
+int main(void)
+{
+		int x = '0';
+
+		while (x <= '9')
+		{
+			putchar(x);
+			if (x != '9')
+			{
+																	putchar(',');
+				putchar(' ');											}			
+			x++;
+		}
+
+		putchar(10);
+		return (0);
+}
