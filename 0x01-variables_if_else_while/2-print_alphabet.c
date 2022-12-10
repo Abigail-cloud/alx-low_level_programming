b01e39717d14f80ee9ef0c69e1fa19fa3a5a795a
Abigail-cloud <toluabby12@gmail.com>
2022-12-09 17:12:15 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: Program to pints to lowercase
+*/
+int main(void)
+{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; ++letter)
+		printf("%c", letter);
+		printf("\n");
+		return (0);
+}
