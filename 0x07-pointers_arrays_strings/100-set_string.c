5732c5906003afae58010f1da0848c3e54610d56
Abigail-cloud <toluabby12@gmail.com>
2023-06-05 20:14:59 -0700

    set string set_string y

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string : function that sets the value of a pointer to a cha
+ * @y: point to an address of string y
+ * @to: the string set the value
+ *
+ * Return: nothing
+ */
+void set_string(char **y, char *to)
+{
+	*y = to;
+}
